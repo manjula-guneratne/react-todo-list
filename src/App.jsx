f4bc750baf61561fe0b4b0afcbbbfe0f51f58054
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-10 18:00:25 -0500

    Use state added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import {useState} from "react"
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e){
+
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form on Submit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input 
+          value={newItem} 
+          onChange = {e => setNewItem(e.target.value)}
+          type="text" 
+          id="item" 
+          />
         </div>
       </form>
       <h1 className="header">Todo List</h1>
