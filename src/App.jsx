c06f73c55a03388162745654f8a6d68c942641cf
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-10 18:46:10 -0500

    onSubmit added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
+import { NewTodoForm } from "./NewTodoForm";
 import "./styles.css";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
+  function addTodo(title) {
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
-
-    setNewItem(""); //Clears the New Item text box
   }
 
   function toggleTodo(id, completed) {
@@ -30,25 +26,15 @@ export default function App() {
     });
   }
 
-  function deleteTodo(id){
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
     <>
-      <form on Submit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            id="item"
-          />
-        </div>
-      </form>
+      <NewTodoForm onSubmit={addTodo}/>
       <h1 className="header">Todo List</h1>
       <ul className="list">
         {todos.length === 0 && "No Todos"}
