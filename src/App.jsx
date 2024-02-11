e86fb0344ba89ff8f922a8a1801885ee29bbc45e
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-10 19:15:00 -0500

    used local storage using UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import "./styles.css";
 import { TodoList } from "./TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+  },[todos])
 
   function addTodo(title) {
     setTodos((currentTodos) => {
